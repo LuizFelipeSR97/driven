c2eeaced785c97eeb76822522fce642218dda436
Luiz <luizfsribeiro97@gmail.com>
2022-06-20 16:51:23 -0300

    Entrega Final sem Bonus

--- projeto4-parrotscardgame/script.js
+++ projeto4-parrotscardgame/script.js
@@ -1,7 +1,13 @@
-let ncartas, i, k, e1, e2;
+//Comentarios:
+
+//Descobrir porque nos numeros pares de acertos uma das cartas segue com a classe 'selecionar'?
+//Falta mudar pras imagens do verso + efeito quando clica na carta
+
+let ncartas, i, k, e1, e2, carta1, carta2, contador, jogadas;
 let j=0;
 let array=[];
 let intervalo=[];
+let idInterval=undefined;
 
 function iniciar(){
 
@@ -44,52 +50,73 @@ function iniciar(){
         carta.classList.remove("c")
         i=i+1
     }
+
+    jogadas=0;
 }
 
 iniciar()
 
 function clicar(elemento){
 
-    if (elemento.classList.contains("certo")==false && elemento.classList.contains("selecionar")==false){
+    if (elemento.classList.contains("certo")==false && elemento.classList.contains("selecionar")==false && idInterval==undefined){
         
         if (j==0){
 
+            carta1=elemento
+
             if (elemento.classList.contains("c0")){
                 c1=0
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/bobrossparrot.gif"
             } else if (elemento.classList.contains("c1")){
                 c1=1
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/explodyparrot.gif"
             } else if (elemento.classList.contains("c2")){
                 c1=2
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/fiestaparrot.gif"
             } else if (elemento.classList.contains("c3")){
                 c1=3
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/metalparrot.gif"
             } else if (elemento.classList.contains("c4")){
                 c1=4
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/revertitparrot.gif"
             } else if (elemento.classList.contains("c5")){
                 c1=5
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/tripletsparrot.gif"
             } else {
                 c1=6
+                carta1.firstChild.src="projeto4-parrotscardgame/midia/unicornparrot.gif"
             }
 
-            carta1=elemento
             carta1.classList.add("selecionar")
             j=1
 
-        } else {
+        } else if (j==1){
+
+            jogadas++
+
+            carta2=elemento
 
             if (elemento.classList.contains("c0")){
                 c2=0
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/bobrossparrot.gif"
             } else if (elemento.classList.contains("c1")){
                 c2=1
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/explodyparrot.gif"
             } else if (elemento.classList.contains("c2")){
                 c2=2
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/fiestaparrot.gif"
             } else if (elemento.classList.contains("c3")){
                 c2=3
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/metalparrot.gif"
             } else if (elemento.classList.contains("c4")){
                 c2=4
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/revertitparrot.gif"
             } else if (elemento.classList.contains("c5")){
                 c2=5
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/tripletsparrot.gif"
             } else {
                 c2=6
+                carta2.firstChild.src="projeto4-parrotscardgame/midia/unicornparrot.gif"
             }
             
             //Se tiver acertado
@@ -148,13 +175,39 @@ function clicar(elemento){
                 }
                 carta1.classList.add("certo")
                 carta2.classList.add("certo")
+            } else {
+                contador=1
+                idInterval=setInterval(desvirar_cartas, 1000);
             }
 
             carta1.classList.remove("selecionar")
 
             j=0
 
-            //Por que nos numeros pares de acertos uma das cartas segue com a classe 'selecionar'?
+            if (document.querySelector(".c0")==null && document.querySelector(".c1")==null && document.querySelector(".c2")==null && document.querySelector(".c3")==null && document.querySelector(".c4")==null && document.querySelector(".c5")==null && document.querySelector(".c6")==null){
+                contador=1
+                idInterval=setInterval(fim_de_jogo, 300);
+            }
         }
     }
+
 }
+
+function desvirar_cartas(){
+    contador--;
+        if (contador === 0) {
+          clearInterval(idInterval)
+          carta1.firstChild.src="projeto4-parrotscardgame/midia/front.png"
+          carta2.firstChild.src="projeto4-parrotscardgame/midia/front.png"
+          idInterval = undefined
+        }
+}
+
+function fim_de_jogo(){
+    contador--;
+        if (contador === 0) {
+          clearInterval(idInterval)
+          alert(`Você ganhou em ${jogadas} jogadas!`)
+        }
+}
+
