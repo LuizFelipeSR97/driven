907a4a76c75f17d033c3bb5bc9eeecd19c060665
Luiz <luizfsribeiro97@gmail.com>
2022-06-26 09:53:39 -0300

    Versao Final

--- projeto5-batepapouol/script.js
+++ projeto5-batepapouol/script.js
@@ -1,22 +1,25 @@
 //Depois tirar o "escondido" do telaEntrada
 
-let i=0, nomeUsuario, mensagem;
+let i=0, j=0, nomeUsuario, mensagem, visibilidade;
 let erroEntrada="sim";
 let paraQuem="Todos";
-let visibilidade="Público";
 
 
 function entrarSessao(){
     
-    nomeUsuario=document.querySelector(".inputNomeUsuario").value
-    const promise1 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",{name: nomeUsuario})
-    promise1.then(sucessoEntrar)
-    promise1.catch(erroEntrar)
-
+    nomeUsuario=document.querySelector(".inputNomeUsuario").value;
+    const promise1 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",{name: nomeUsuario});
+    promise1.then(sucessoEntrar);
+    promise1.catch(erroEntrar);
+    paraQuem="Todos";
+    visibilidade="Público"
 }
 
 function sucessoEntrar(sucessoEntrar){
     document.querySelector(".telaEntrada").classList.add("escondido")
+    setInterval(manterConexao,5000)
+    buscarMensagens();
+    setInterval(buscarMensagens,3000);
 }
 
 function erroEntrar(erroEntrar){
@@ -24,19 +27,28 @@ function erroEntrar(erroEntrar){
     document.querySelector(".inputNomeUsuario").value=""
 }
 
-function buscarMensagens(){
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
-    promessa.then(processarResposta)
+function manterConexao(){
+    promiseManterConexao=axios.post("https://mock-api.driven.com.br/api/v6/uol/status",{name: nomeUsuario})
+    promiseManterConexao.catch(manterConexao)
 }
 
-//buscarMensagens();
+function buscarMensagens(){
+    const promiseMsg = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promiseMsg.then(processarRespostaMsg);
+    const promiseMembros = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
+    promiseMembros.then(processarRespostaMembros);
+}
 
-function processarResposta(resp){
+function processarRespostaMsg(resp){
     renderizarMensagens(resp.data);
-    setTimeout(buscarMensagens,3000);
+}
+
+function processarRespostaMembros(resp){
+    renderizarMembros(resp.data);
 }
 
 function renderizarMensagens(mensagens){
+    document.querySelector(".telaMensagens .conteudo").innerHTML=""
     for (i==0; i<mensagens.length; i++) {
         if (mensagens[i].type==="status"){
             document.querySelector(".telaMensagens .conteudo").innerHTML+=`<div class="caixaMsg entrouSaiu">
@@ -46,13 +58,31 @@ function renderizarMensagens(mensagens){
             document.querySelector(".telaMensagens .conteudo").innerHTML+=`<div class="caixaMsg">
             <h1>(${mensagens[i].time})</h1><h2>${mensagens[i].from}</h2><h3>para</h3><h2>${mensagens[i].to}:</h2><h3>${mensagens[i].text}</h3>
           </div>`
-        } else if (mensagens[i].type==="private_message"){
+        } else if (mensagens[i].type==="private_message" && (mensagens[i].to===nomeUsuario || mensagens[i].from===nomeUsuario )){
             document.querySelector(".telaMensagens .conteudo").innerHTML+=`<div class="caixaMsg reservado">
             <h1>(${mensagens[i].time})</h1><h2>${mensagens[i].from}</h2><h3>reservadamente para</h3><h2>${mensagens[i].to}:</h2><h3>${mensagens[i].text}</h3>
           </div>`
         }
     }
-    console.log(mensagens)
+    document.querySelector(".telaMensagens .conteudo").innerHTML+=`<div class="preencherEspaco"></div>`
+    document.querySelector(".telaMensagens .conteudo").lastChild.scrollIntoView()
+}
+
+function renderizarMembros(membros){
+    
+    
+    if (paraQuem=="Todos"){
+        document.querySelector(".menuLateral .paraQuem").innerHTML=`<div class='pessoa' onclick='escolherDestinatario(this)'><img src="projeto5-batepapouol/img/iconMembros.svg" height="15px"/><h1>Todos</h1><img src="projeto5-batepapouol/img/check.svg" height="10px" class="check mostrarCheck"/></div>`
+    } else {
+        document.querySelector(".menuLateral .paraQuem").innerHTML=`<div class='pessoa' onclick='escolherDestinatario(this)'><img src="projeto5-batepapouol/img/iconMembros.svg" height="15px"/><h1>Todos</h1><img src="projeto5-batepapouol/img/check.svg" height="10px" class="check"/></div>`
+    }
+    for (i=0; i<membros.length; i++) {
+        if (membros[i].name==paraQuem){
+            document.querySelector(".menuLateral .paraQuem").innerHTML+=`<div class='pessoa' onclick='escolherDestinatario(this)'><img src="projeto5-batepapouol/img/iconMembros.svg" height="15px"/><h1>${membros[i].name}</h1><img src="projeto5-batepapouol/img/check.svg" height="10px" class="check mostrarCheck"/></div>`
+        } else {
+            document.querySelector(".menuLateral .paraQuem").innerHTML+=`<div class='pessoa' onclick='escolherDestinatario(this)'><img src="projeto5-batepapouol/img/iconMembros.svg" height="15px"/><h1>${membros[i].name}</h1><img src="projeto5-batepapouol/img/check.svg" height="10px" class="check"/></div>`
+        }
+    }
 }
 
 function abrirFecharMenuLateral(){
@@ -75,11 +105,23 @@ function enviarMsgReservada(){
 
 function enviarMsg(){
     mensagem=document.querySelector(".textoMensagem").value
-    alert(`Vai aparecer na tela a mensagem '${mensagem}'`)
-    //axios.post("")
-    //ultima etapa: apagar o texto que tava
-    document.querySelector(".textoMensagem").value=""
-
+    if (visibilidade==="Público"){
+        msgDigitada={
+            from: nomeUsuario,
+            to: paraQuem,
+            text: mensagem,
+            type: "message"
+        }
+    } else if (visibilidade==="Reservadamente"){
+        msgDigitada={
+            from: nomeUsuario,
+            to: paraQuem,
+            text: mensagem,
+            type: "private_message"
+        }
+    } 
+    const promiseMsgDigitada=axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",msgDigitada)
+    promiseMsgDigitada.then(document.querySelector(".textoMensagem").value="")
 }
 
 function escolherDestinatario(opcao){
@@ -95,12 +137,8 @@ function escolherDestinatario(opcao){
 
 function escolherVisibilidade(opcao){
     if (paraQuem!=="Todos"){
-        document.querySelector(".mostrarCheck2").classList.remove("mostrarCheck2")
-        opcao.lastElementChild.classList.add("mostrarCheck2")
-        visibilidade=(opcao.lastElementChild.previousElementSibling.innerHTML)
+        document.querySelector(".mostrarCheck2").classList.remove("mostrarCheck2");
+        opcao.lastElementChild.classList.add("mostrarCheck2");
+        visibilidade="Reservadamente";
     }
 }
-
-//Funcoes para executarem ao iniciar
-
-//buscarMensagens();
